fb254c0b65065532680fdb564207968ce7f52b8d
Waleed Almotairi <AlmotairiWal@gmail.com>
2021-04-07 18:28:54 +0300

    commit 6:28 pm

--- test.js
+++ test.js
@@ -1,41 +1,55 @@
 $(document).ready(function(){
+    $(".hungry").hide();
+    $(".sad").hide();
+    var countSad = 0 ;
+    var countHu = 0;
+    $("#btnSad").click(function(event ) {
+       if(countSad > 0)
+       {
+        $(".sad").empty()
+       }
+       // $(".sad").off(event);
+        $(".hungry").hide();
+        $.get("https://api.chucknorris.io/jokes/random",function(response){
+        console.log(response);
+        $(".sad").append("<img src='" + response.icon_url + "'>");
+        $(".sad").append("<p id = 'pSad'>"+ response.value + "</p>");
+        $(".sad").show();
+        countSad++;
+     });
+    });
     
-    const settings = {
+
+    
+    $("#btnHun").click(function() {
+        if(countHu > 0)
+       {
+        $(".hungry").empty()
+       }
+    $(".sad").hide();
+    const settings2 = {
     "async": true,
     "crossDomain": true,
-    "url": "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
+    "url": "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple",
     "method": "GET",
     "headers": {
-    "accept": "application/json",
     "x-rapidapi-key": "9a0abae874msh2da2d3068725f2ep1a9982jsn114baac1ccb5",
-    "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
-}
-};
-$.ajax(settings).done(function (response) {
-console.log(response);
-console.log(response.icon_url);
-$(".API").append("<img src='" + response.icon_url + "'>");
-$(".API").append(response.value);
-});
-
-const settings2 = {
-"async": true,
-"crossDomain": true,
-"url": "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple",
-"method": "GET",
-"headers": {
-"x-rapidapi-key": "9a0abae874msh2da2d3068725f2ep1a9982jsn114baac1ccb5",
-"x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com"
+    "x-rapidapi-host": "edamam-food-and-grocery-database.p.rapidapi.com"
 }
 };
 
 $.ajax(settings2).done(function (response) {
 
-$(".API").append("<img src='" + response.hints[0].food.image + "'>")
-$(".API").append("<p> " + response.hints[0].food.label+ "<p>");
-$(".API").append("<p> " + response.hints[0].food.nutrients.CHOCDF+ "<p>");
-
+    $(".hungry").append("<img src='" + response.hints[0].food.image + "'>")
+   
+    $(".hungry").append("<p id = 'pHun'> " + response.hints[0].food.label+" <br>"+ response.hints[0].food.nutrients.CHOCDF+ "<p>");
+    
+    // $(".hungry").append("<p> " +  "<p>");
+    $(".hungry").show();
+    countHu++;
 
 });
+    });
+
 
 });
